6d7295099642796588ae37bf5f162f8394532ed9
rakeshyadav906 <rakeshyadavn906@gmail.com>
2026-08-08 22:19:41 +0530

    Use environment variables for MongoDB and port

--- db.js
+++ db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://rakeshyadavn906_db_user:Rakesh906@cluster0.b6m9lyn.mongodb.net/foodfinder?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("✅ MongoDB Connected");
   } catch (err) {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 
+
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db");
@@ -7,7 +8,7 @@ connectDB();
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const restaurantRoutes = require("./routes/restaurants");
 
